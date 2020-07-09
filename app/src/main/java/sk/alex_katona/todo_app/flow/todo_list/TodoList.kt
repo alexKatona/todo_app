73a2db95d6596ec8e2de62ce937cbe2e3fc1dca9
alexander.katona <alexander.katona@wirecard.com>
2020-07-09 14:07:34 +0200

    added formatting

--- app/src/main/java/sk/alex_katona/todo_app/flow/todo_list/TodoList.kt
+++ app/src/main/java/sk/alex_katona/todo_app/flow/todo_list/TodoList.kt
@@ -1,16 +1,18 @@
-package sk.alex_katona.todo_app
+package sk.alex_katona.todo_app.flow.todo_list
 
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.lifecycleScope
 import kotlinx.android.synthetic.main.fragment_first.*
+import sk.alex_katona.todo_app.R
 import sk.alex_katona.todo_app.base.BaseFragment
 import sk.alex_katona.todo_app.helpers.clicksThrottled
 import sk.alex_katona.todo_app.navigators.AppScreens
 
 class TodoList : BaseFragment() {
 
-    override fun getLayoutResId(): Int = R.layout.fragment_first
+    override fun getLayoutResId(): Int =
+        R.layout.fragment_first
 
     override fun init(view: View, savedInstanceState: Bundle?) {
         button_first.clicksThrottled(lifecycleScope) {
